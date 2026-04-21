d3af97c4101bff44abe0a4f8f28781f15ca325c8
kristimarkus-tlu <kristimarkus@gmail.com>
2026-04-21 04:34:50 +0300

    rm extra

--- config/supabase.js
+++ config/supabase.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 
 const { createClient } = require("@supabase/supabase-js");
 
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY || process.env.SUPABASE_KEY;
-
-if (!process.env.SUPABASE_URL || !supabaseKey) {
+if (!process.env.SUPABASE_URL || !process.env.SUPABASE_SERVICE_KEY) {
     throw new Error("Supabase env variables are missing.");
 }
 
-const supabase = createClient(process.env.SUPABASE_URL, supabaseKey);
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_KEY
+);
 
 module.exports = supabase;
